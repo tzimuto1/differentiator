f1b9ce956c9dda3e0d03ab9fc6adecaa7e772377
Tawanda Zimuto <tzimuto1@gmail.com>
2015-06-20 21:24:45 -0400

    Add some methods for the parser

--- src/core/Parser.java
+++ src/core/Parser.java
@@ -1,7 +1,9 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
+import core.Token.Type;
 import core.expression.*;
 
 /**
@@ -12,6 +14,7 @@ import core.expression.*;
 public class Parser {
 	
 	ArrayList<Token> tokens;
+	ListIterator<Token> iter;
 	
 	/**
 	 * The mathematical expression parser
@@ -19,16 +22,119 @@ public class Parser {
 	 */
 	public Parser(Lexer lexer) {
 		tokens = lexer.getTokens();
+		iter = tokens.listIterator();
 	}
 	
+	/**
+	 * Parses the full mathematical expression
+	 * @return Expression
+	 */
+	public Expression parse() {
+		if (iter.hasNext()){
+			Token token = iter.next();
+			if (token.getType() == Type.OPEN_PARENTH) return parseExpression();
+			else throw new RuntimeException("The expression format invalid");
+		}
+		throw new RuntimeException("The expression format invalid");
+	}
+	
+	/**
+	 * Parses an expression that starts with an open parenthesis
+	 * @return Expression
+	 */
+	private Expression parseExpression(){
+		Expression left;
+		while(iter.hasNext()){
+			Token token = iter.next();
+			if (token.getType() == Type.VARIABLE){
+				left = new Variable(token.getValue());
+				return handleLeftTerminal(left);
+			} else if (token.getType() == Type.NUMBER){
+				left = new core.expression.Number(token.getValue());
+				return handleLeftTerminal(left);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param left the already parsed number of variable
+	 * @return Expression
+	 */
+	private Expression handleLeftTerminal(Expression left){
+		if (iter.hasNext()){
+			Token nextToken = iter.next();
+			if (nextToken.getType() == Type.CLOSED_PARENTH){
+				return left;
+			} else if (nextToken.getType() == Type.PLUS){
+				return parseAdd(left);
+			} else if (nextToken.getType() == Type.MINUS){
+				return parseSubtract(left);
+			} else if (nextToken.getType() == Type.MULTI){
+				return parseMultiply(left);
+			} 
+		}
+		throw new RuntimeException("The expression format invalid");
+	}
 	/**
 	 * Parses the Add expression
 	 * @param left the already parsed left expression of the Add expression
 	 * @return Expression whose concrete type is Add
 	 */
 	public Expression parseAdd(Expression left) {
-		// TODO Auto-generated method stub
-		return null;
+		Expression right = parseOperatorHelper();
+		return new Add(left, right);
+	}
+	
+	/**
+	 * Parses the Subtract expression
+	 * @param left the already parsed left expression of the Subtract expression
+	 * @return Expression whose concrete type is Subtract
+	 */
+	public Expression parseSubtract(Expression left) {
+		Expression right = parseOperatorHelper();
+		return new Subtract(left, right);
+	}
+	
+	/**
+	 * Parses the Multiply expression
+	 * @param left the already parsed left expression of the Multiply expression
+	 * @return Expression whose concrete type is Multiply
+	 */
+	public Expression parseMultiply(Expression left) {
+		Expression right = parseOperatorHelper();
+		return new Multiply(left, right);
+	}
+	
+	/**
+	 * Parses the right part of an Add expression
+	 * @return Expression
+	 */
+	private Expression parseOperatorHelper(){
+		while(iter.hasNext()){
+			Token token = iter.next();
+			if (token.getType() == Type.NUMBER){
+				if (isExpressionTerminated())return new core.expression.Number(token.getValue());
+				else throw new RuntimeException("The expression format invalid");
+			} else if (token.getType() == Type.VARIABLE){
+				if (isExpressionTerminated()) return new Variable(token.getValue());
+				else throw new RuntimeException("The expression format invalid");
+			}
+			//TODO test other parts of the expression
+		}
+		throw new RuntimeException("The expression format invalid");
+	}
+	/**
+	 * Determines if the current expression is correctly terminated. A valid expression is
+	 * is terminated by a parenthesis 
+	 * @return boolean 
+	 */
+	private boolean isExpressionTerminated(){
+		if (iter.hasNext()){
+			Token nextToken = iter.next();
+			return nextToken.getType() == Type.CLOSED_PARENTH;
+		} else return false;
 	}
 
 }
--- src/test/core/ParserTest.java
+++ src/test/core/ParserTest.java
@@ -10,17 +10,174 @@ import core.expression.*;
 
 public class ParserTest {
 	//Tests
-	//	parseAdd(Expression left):
-	//		expr = (x+, 3), (3+ x), (3+ ( 
-	//
+	//	parse[Add/Subtract/Multiply](Expression left):
+	//      These test cases cover the test cases for the parseOperatorHelper() method. Thus
+	//      tests that use the "-" and "*" operators have been left out
+	//		expr = (x+, 3), (3+ x), (x+ 3 illegal, (3+ x illegal, (x + 3, (3 + x
+	
+	//TODO clean up the first set of tests
 	@Test
-	public void testAddNumVar(){
-		Parser parser = new Parser(new Lexer("3)"));
+	// (x + 3)
+	public void testParseAddVarNum(){
 		Expression left = new Variable("x");
+		Parser parser = new Parser(new Lexer("3)"));
 		Expression actualExpr = parser.parseAdd(left);
 		//expected
 		Expression expectedExpr = new Add(new Variable("x"), new core.expression.Number("3"));
 		//assert
-		assertEquals(expectedExpr, actualExpr);
+		assertEquals(expectedExpr.toString(), actualExpr.toString());
 	}
+	
+	@Test
+	// (3 + x)
+	public void testParseAddNumVar(){
+		Expression left = new core.expression.Number("3");
+		Parser parser = new Parser(new Lexer("x)"));//right part of expression
+		Expression actualExpr = parser.parseAdd(left);
+		//expected
+		Expression expectedExpr = new Add(new core.expression.Number("3"), new Variable("x"));
+		//assert
+		assertEquals(expectedExpr.toString(), actualExpr.toString());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	// (x + 3*
+	public void testParseAddVarNumIllegal(){
+		Expression left = new Variable("x");
+		Parser parser = new Parser(new Lexer("3*"));
+		parser.parseAdd(left);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	// (3 + x*
+	public void testParseAddNumVarIllegal(){
+		Expression left = new core.expression.Number("3");
+		Parser parser = new Parser(new Lexer("x*"));
+		parser.parseAdd(left);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	// (x + 3
+	public void testParseAddVarNumEmpty(){
+		Expression left = new Variable("x");
+		Parser parser = new Parser(new Lexer("3"));
+		parser.parseAdd(left);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	// (3 + x
+	public void testParseAddNumVarEmpty(){
+		Expression left = new core.expression.Number("3");
+		Parser parser = new Parser(new Lexer("x"));
+		parser.parseAdd(left);
+	}
+	
+	//  parse() It implicitly covers the parseExpression() method:
+		//  single variable (x), number (3), 2 single-layer expression plus (x + 3), 
+	    //  2 single-layer expression subtract (x - 3), 2 single-layer expression multi (x * 3)
+		//	double layer expression [(x + (x - 3)), ((x-3) + 2), ((x +1) + (x + 2))],
+	    //deeply nested expression (((x + (3 * 4)) + 3) -3)
+		//  unnecessary brackets1 (((x))), unnecessary brackets 2 ((x +3))
+	
+	@Test
+	// (x)
+	public void testParseSingleVariable(){
+		//actual
+		Parser parser = new Parser(new Lexer("(x)"));
+		Expression actual = parser.parse();
+		
+		String expected = "x";
+		assertEquals(expected, actual.toString());
+		
+	}
+	
+	@Test
+	// (3)
+	public void testParseSingleNumber(){
+		//actual
+		Parser parser = new Parser(new Lexer("(3)"));
+		Expression actual = parser.parse();
+		
+		String expected = "3";
+		assertEquals(expected, actual.toString());	
+	}
+	
+	@Test
+	// (x + 3)
+	public void testParseSingleLayerAddExpression1(){
+		//actual
+		Parser parser = new Parser(new Lexer("(x + 3)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(x+3)";
+		assertEquals(expected, actual.toString());
+	}
+	
+	@Test
+	// (3 + x)
+	public void testParseSingleLayerAddExpression2(){
+		//actual
+		Parser parser = new Parser(new Lexer("(3 + x)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(3+x)";
+		assertEquals(expected, actual.toString());
+	}
+	
+	@Test
+	// (x - 3)
+	public void testParseSingleLayerSubtractExpression1(){
+		//actual
+		Parser parser = new Parser(new Lexer("(x - 3)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(x-3)";
+		assertEquals(expected, actual.toString());
+		
+	}
+	
+	@Test
+	// (3 - x)
+	public void testParseSingleLayerSubtractExpression2(){
+		//actual
+		Parser parser = new Parser(new Lexer("(3 - x)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(3-x)";
+		assertEquals(expected, actual.toString());
+		
+	}
+	
+	@Test
+	// (x * 3)
+	public void testParseSingleLayerMultiplyExpression1(){
+		//actual
+		Parser parser = new Parser(new Lexer("(x * 3)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(x*3)";
+		assertEquals(expected, actual.toString());
+	}
+	
+	@Test
+	// (3 * x)
+	public void testParseSingleLayerMultiplyExpression2(){
+		Parser parser = new Parser(new Lexer("(3 * x)"));
+		Expression actual = parser.parse();
+		
+		String expected = "(3*x)";
+		assertEquals(expected, actual.toString());
+	}
+	
+//	[(x + (x - 3)), ((x-3) + 2), ((x +1) + (x + 2))],
+	@Test
+	//(x + (x - 3))
+	public void doubleLayerExpression1(){
+		Parser parser = new Parser(new Lexer("(x + (x - 3))"));
+		Expression actual = parser.parse();
+		
+		String expected = "(x+(x-3))";
+		assertEquals(expected, actual.toString());
+	}
+	
 }
